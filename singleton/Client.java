c36eccdd7cea3ff85086f23b06d2fef9623345a9
waonderboy <crazy7523@gmail.com>
2022-03-14 18:07:04 +0900

    singleton

--- singleton/Client.java
+++ singleton/Client.java
@@ -1,8 +1,6 @@
 package singleton;
 
-import singleton.Settings;
-
-public class Main {
+public class Client {
     public static void main(String args[]) {
         Settings instance1 = Settings.getInstance();
         Settings instance2 = Settings.getInstance();
