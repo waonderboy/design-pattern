04637e981d57cf240e52fa9c78559474b3177a75
waonderboy <crazy7523@gmail.com>
2022-03-14 18:24:40 +0900

    Proxy

--- proxy/restrict/Client.java
+++ proxy/restrict/Client.java
@@ -4,6 +4,7 @@ public class Client {
     public static void main(String args[]) throws Exception {
         Internet site = new ProxyInternet(new RealInternet());
 
+        // connect
         site.connectTo("nabor.com");
         site.connectTo("bat777.com");
     }
