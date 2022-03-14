ce018c19487baf0140734a53e64ef0dc8efdc47b
waonderboy <crazy7523@gmail.com>
2022-03-14 18:07:51 +0900

    Singleton

--- singleton/Client.java
+++ singleton/Client.java
@@ -2,6 +2,7 @@ package singleton;
 
 public class Client {
     public static void main(String args[]) {
+        // make instance
         Settings instance1 = Settings.getInstance();
         Settings instance2 = Settings.getInstance();
         System.out.println(instance1 == instance2);
